23cb31f3554e12c8ff90bbbc3b27a83401acad09
Washington gabriel <washingtongabrielps@gmail.com>
2025-07-07 16:33:04 +0000

    podemos deixar essa sessão assim no mobile também? agrupados

--- src/components/landing/Advantages.tsx
+++ src/components/landing/Advantages.tsx
@@ -38,7 +38,7 @@ export const Advantages = () => {
           </div>
         </div>
         
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-5xl mx-auto">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4 max-w-5xl mx-auto">
           {advantages.map((advantage, index) => (
             <div 
               key={index} 
